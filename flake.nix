ccd91310746124570e8f0c3f4b432c57eb600e6f
Yusong <yusonglai64@gmail.com>
2025-06-21 22:50:10 +0800

    build: update rust toolchain in flake.nix

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
           #   extensions = [ "rust-src" ];
           #   targets = [ ];
           # };
-          rust.nightly."2025-02-20".default.override {
+          rust.nightly."2025-06-20".default.override {
             extensions = [ "rust-src" ];
             targets = [ ];
           };
@@ -58,11 +58,11 @@
         {
           default = pkgs.mkShell {
             packages = with pkgs; [
-              protobuf
               rustToolchain
-              cargo-edit
-              bacon
-              just
+              cargo-edit # managing cargo dependencies
+              bacon # background code checker
+              just # just a command runner
+              protobuf
             ];
           };
         }
